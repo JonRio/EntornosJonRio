a6e76eaddac6fd145623a29a1e19b46b319e2e9b
Txunio <Jon Txunio>
2017-05-02 23:28:13 +0200

    Cambio la syso de resta y le añado otra s de mas.

--- EntornosJonRio/src/PruebaJR.java
+++ EntornosJonRio/src/PruebaJR.java
@@ -5,7 +5,7 @@ public class PruebaJR {
 		int n1=2,n2=3;
 		System.out.println("Este es un programa");
 		System.out.println("El resultado de la suma es: "+n1+n2);//Suma dos numeros
-		System.out.println("El resultado de la resta es: "+(n1-n2));
+		System.out.println("El resultado de la resta ess: "+(n1-n2));
 	}
 
 }
