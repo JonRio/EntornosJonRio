c2930a2873a62ecd638c740ac199f8b33c276a61
Txunio <Jon Txunio>
2017-05-02 22:53:46 +0200

    Version original.

---
+++ EntornosJonRio/src/PruebaJR.java
@@ -0,0 +1,10 @@
+
+public class PruebaJR {
+
+	public static void main(String[] args) {
+		int n1=2,n2=3;
+		System.out.println("Este es un programa");
+		System.out.println("El resultado de la suma es: "+n1+n2);//Suma dos numeros
+	}
+
+}
